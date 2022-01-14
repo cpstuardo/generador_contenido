e8df4b0f8bfaf3cd88b4a62c6f6cc68d44f66008
cpstuardo <cpstuardo@uc.cl>
2022-01-14 14:53:54 -0300

    cambios diseño

--- src/App.js
+++ src/App.js
@@ -1,54 +1,89 @@
 import "./App.css";
 import ComponentToPrint from "./components/componentToPrint";
-import React, { useCallback, useRef } from "react";
+import React, { useRef, useState } from "react";
 import { PDFExport } from "@progress/kendo-react-pdf";
 import { toPng } from "html-to-image";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import { Container, Row, Col } from "reactstrap";
 
 function App() {
-  const ref = useRef();
+  const [fileName, setFileName] = useState("post");
+  const pngExportComponent = useRef();
   const pdfExportComponent = useRef(null);
 
-  const exportComponentAsPDF = (event) => {
+  const exportComponentAsPDF = (e) => {
     pdfExportComponent.current.save();
   };
 
-  const exportComponentAsPNG = useCallback(() => {
-    if (ref.current === null) {
+  const exportComponentAsPNG = () => {
+    if (pngExportComponent.current === null) {
       return;
     }
-    toPng(ref.current, {
+    toPng(pngExportComponent.current, {
       cacheBust: true,
       canvasWidth: 1080,
       canvasHeight: 1920,
     })
       .then((dataUrl) => {
         const link = document.createElement("a");
-        link.download = "my-image-name.png";
+        link.download = fileName;
         link.href = dataUrl;
         link.click();
       })
       .catch((err) => {
         console.log(err);
       });
-  }, [ref]);
+  };
 
   return (
     <div className="App">
-      <br />
-      <div className="export-container">
-        <PDFExport
-          ref={pdfExportComponent}
-          paperSize="auto"
-          margin={30}
-          fileName={"component_pdf"}
-          author="Yachay"
+      <Container className="app-container">
+        <Row
+          style={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "row",
+          }}
         >
-          <ComponentToPrint ref={ref} />
-        </PDFExport>
-      </div>
-      <br />
-      <button onClick={exportComponentAsPNG}>Exportar PNG</button>
-      <button onClick={exportComponentAsPDF}>Exportar PDF</button>
+          <Col className="col-exportable">
+            <PDFExport
+              ref={pdfExportComponent}
+              paperSize="auto"
+              margin={30}
+              fileName={fileName}
+              author="Yachay"
+            >
+              <ComponentToPrint ref={pngExportComponent} />
+            </PDFExport>
+          </Col>
+          <Col className="col-options">
+            <TextField
+              id="outlined-search"
+              label="Nombre del archivo"
+              size="small"
+              value={fileName}
+              onChange={(e) => {
+                setFileName(e.target.value);
+              }}
+            />
+            <br />
+            <br />
+            <Button
+              onClick={exportComponentAsPNG}
+              style={{ color: "white", backgroundColor: "#CDCDCD", margin: 2 }}
+            >
+              Exportar PNG
+            </Button>
+            <Button
+              onClick={exportComponentAsPDF}
+              style={{ color: "white", backgroundColor: "#CDCDCD", margin: 2 }}
+            >
+              Exportar PDF
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
